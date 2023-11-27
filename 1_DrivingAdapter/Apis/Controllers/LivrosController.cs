b71a8fb30acb53bb6a4eeb64e0eccd227b521c80
Raazak <rlalves4880@gmail.com>
2023-11-27 20:00:16 -0300

    api e acesso ao banco

---
+++ 1_DrivingAdapter/Apis/Controllers/LivrosController.cs
@@ -0,0 +1,18 @@
+﻿using ExemploAplication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivrosController : ControllerBase
+    {
+        [Route("ObterLivros")]
+        [HttpGet]
+        public string ObterLivros()
+        {
+            return new LivrosApp().ObterLivros();
+        }
+    }
+}
---
+++ 1_DrivingAdapter/Apis/Controllers/saudacaoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Apis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class saudacaoController : ControllerBase
+    {
+        [HttpGet]
+        public string Alo() => "Alo mundo.";
+    }
+}
