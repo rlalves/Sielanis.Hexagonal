50ee199ed258af4c9a19498c3003195de5f872bd
Raazak <rlalves4880@gmail.com>
2023-11-27 21:53:46 -0300

    correção no nome do projeto

--- 1_DrivingAdapter/Apis/Controllers/LivrosController.cs
+++ 1_DrivingAdapter/Apis/Controllers/LivrosController.cs
@@ -1,4 +1,4 @@
-﻿using ExemploAplication;
+﻿using Aplication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
---
+++ 2_Aplication/Aplication/LivrosApp.cs
@@ -0,0 +1,24 @@
+﻿using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication
+{
+    public class LivrosApp
+    {
+        public string ObterLivros()
+        {
+            List<string> livros = new LivrosRepository().Obterlivros();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (string livro in livros)
+                sb.Append(livro + ", ");
+
+            string r = sb.ToString();
+            return r.Substring(0, r.Length - 2);
+        }
+    }
+}
