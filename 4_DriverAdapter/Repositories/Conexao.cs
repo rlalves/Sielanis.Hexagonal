7a0fd041ef13739cb93a4706a5a72329186643a7
Raazak <rlalves4880@gmail.com>
2023-11-27 22:21:17 -0300

    correção no nome do projeto

---
+++ 4_DriverAdapter/Repositories/Conexao.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class Conexao
+    {
+        private const string StringDeConexao = "server=localhost;uid=root;pwd=root;database=Sielanis;";
+        private MySqlConnection conexao;
+
+        public Conexao()
+        {
+            conexao = new MySqlConnection();
+        }
+
+        public void Abrir()
+        {
+            try
+            {
+                conexao = new MySqlConnection(StringDeConexao);
+                conexao.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro de conexão: " + ex.Message);
+            }
+        }
+
+        public void Fechar()
+        {
+            conexao.Close();
+        }
+
+        public MySqlConnection ObtemConexao() => conexao;
+    }
+}
---
+++ 4_DriverAdapter/Repositories/LivrosRepository.cs
@@ -0,0 +1,41 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class LivrosRepository
+    {
+        Conexao conexao = new Conexao();
+
+        public List<string> Obterlivros()
+        {
+            try
+            {
+                List<string?> livros = new List<string?>();
+
+                conexao.Abrir();
+                string query = "SELECT nome FROM livros";
+
+                MySqlCommand cmd = new MySqlCommand(query, conexao.ObtemConexao());
+                MySqlDataReader r = cmd.ExecuteReader();
+
+                while (r.Read())
+                {
+                    livros.Add(r["nome"].ToString());
+                }
+
+                conexao.Fechar();
+
+                return livros;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
